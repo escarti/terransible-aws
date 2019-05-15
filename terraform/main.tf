034fd93bb680756d2a3607173862d4639e5336f9
Eduardo Escarti <eduardo.escarti@nb-eescarti.local>
2019-05-15 12:20:23 +0200

    fixed missing $

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ resource "aws_iam_instance_profile" "s3_access_profile" {
 
 resource "aws_iam_role_policy" "s3_access_policy" {
   name = "s3_access_policy"
-  role = "{aws_iam_role.s3_access_role.id}"
+  role = "${aws_iam_role.s3_access_role.id}"
   policy = <<EOF
 {
   "Version": "2019-05-15",
