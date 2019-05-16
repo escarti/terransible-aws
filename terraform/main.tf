ff99d92d43a9e8a3f9a903614a649baf53b2527d
root <root@eduardoescarti1c.mylabserver.com>
2019-05-16 10:08:23 +0000

    Format corrected

--- terraform/main.tf
+++ terraform/main.tf
@@ -519,35 +519,34 @@ resource "aws_autoscaling_group" "wp_asg" {
 # Primary zone
 
 resource "aws_route53_zone" "primary" {
-  name = "${var.domain_name}.${var.domain_ending}"
+  name              = "${var.domain_name}.${var.domain_ending}"
   delegation_set_id = "${var.delegation_set}"
 }
 
 resource "aws_route53_record" "www" {
   zone_id = "${aws_route53_zone.primary.id}"
-  name = "www.${var.domain_name}.${var.domain_ending}"
-  type = "A"
+  name    = "www.${var.domain_name}.${var.domain_ending}"
+  type    = "A"
 
   alias {
-    name = "${aws_elb.wp_elb.dns_name}"
-    zone_id = "${aws_elb.wp_elb.zone_id}"
+    name                   = "${aws_elb.wp_elb.dns_name}"
+    zone_id                = "${aws_elb.wp_elb.zone_id}"
     evaluate_target_health = false
-  }  
-
+  }
 }
 
 resource "aws_route53_record" "dev" {
   zone_id = "${aws_route53_zone.primary.id}"
-  name = "dev.${var.domain_name}.${var.domain_ending}"
-  type = "A"
-  ttl = "300"
-  records = ["${aws_instance.wp_dev.public_ip}"]  
+  name    = "dev.${var.domain_name}.${var.domain_ending}"
+  type    = "A"
+  ttl     = "300"
+  records = ["${aws_instance.wp_dev.public_ip}"]
 }
 
 # Private zone (secondary)
 
 resource "aws_route53_zone" "secondary" {
-  name = "${var.domain_name}.${var.domain_ending}"
+  name   = "${var.domain_name}.${var.domain_ending}"
   vpc_id = "${aws_vpc.wp_vpc.id}"
 }
 
@@ -555,9 +554,8 @@ resource "aws_route53_zone" "secondary" {
 
 resource "aws_route53_record" "db" {
   zone_id = "${aws_route53_zone.secondary.zone_id}"
-  name = "db.${var.domain_name}.${var.domain_ending}"
-  type = "CNAME"
-  ttl = "300"
+  name    = "db.${var.domain_name}.${var.domain_ending}"
+  type    = "CNAME"
+  ttl     = "300"
   records = ["${aws_db_instance.wp_db.address}"]
 }
-
